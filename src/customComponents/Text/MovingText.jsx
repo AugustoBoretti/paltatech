3b689cf52144b2b316d61fc72f4228e6fac971ab
atidele <alejandro.tidele@mercadolibre.com>
2020-06-19 11:52:52 -0300

    Auto text change

--- src/customComponents/Text/MovingText.jsx
+++ src/customComponents/Text/MovingText.jsx
@@ -1,46 +1,27 @@
 import React from "react";
+import Typed from "typed.js";
 
 const MovingText = ({ className }) => {
-  const textToWrite = ["TECH", "TEAM"];
-  const [text, setText] = React.useState("");
-
-  const textProps = {
-    isDeleting: false,
-    loopNum: 0,
-    typingSpeed: 450,
-    text: "",
-  };
-
   React.useEffect(() => {
-    setTimeout(autoWriteText, 1000);
+    new Typed("#typed", {
+      stringsElement: "#typed-strings",
+      typeSpeed: 250,
+      loop: true,
+      smartBackspace: false,
+      backSpeed: 150,
+      startDelay: 1000,
+    });
   }, []);
-
-  const autoWriteText = () => {
-    const fullText = textToWrite[textProps.loopNum % textToWrite.length];
-    const partialText = textProps.isDeleting
-      ? fullText.substring(0, textProps.text.length - 1)
-      : fullText.substring(0, textProps.text.length + 1);
-
-    textProps.typingSpeed = textProps.isDeleting ? 300 : 450;
-
-    setText(partialText);
-
-    textProps.text = partialText;
-
-    if (!textProps.isDeleting && textProps.text === fullText) {
-      setTimeout(() => (textProps.isDeleting = true), 1000);
-    } else if (textProps.isDeleting && textProps.text === "") {
-      textProps.isDeleting = false;
-      textProps.loopNum++;
-    }
-
-    setTimeout(autoWriteText, textProps.typingSpeed);
-  };
 
   return (
     <div className={`rez-text ${className}`}>
       <span className="text-white fs-85">PALTA</span>
-      <span className="text-success fs-85">{text}</span>
+      <span id="typed-strings">
+        <div>TECH</div>
+        <div>TEAM</div>
+        <div>PRO</div>
+      </span>
+      <span className="text-success fs-85" id="typed"></span>
     </div>
   );
 };

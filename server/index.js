6f574239ee818cecb53912c40a32cc9886df257c
atidele <alejandro.tidele@mercadolibre.com>
2020-06-21 14:42:12 -0300

    HTTPS is now available! 🚀

--- server/index.js
+++ server/index.js
@@ -1,13 +1,41 @@
 const path = require("path");
+const http = require('http');
+const https = require('https');
 const express = require("express");
 const app = express(); // create express app
+
+const privateKey = fs.readFileSync(
+  "/etc/letsencrypt/live/yourdomain.com/privkey.pem",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  "/etc/letsencrypt/live/yourdomain.com/cert.pem",
+  "utf8"
+);
+const ca = fs.readFileSync(
+  "/etc/letsencrypt/live/yourdomain.com/chain.pem",
+  "utf8"
+);
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
 
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 app.use(express.static(__dirname, { dotfiles: "allow" }));
 
-// start express server on port 80
-app.listen(80, () => {
-  console.log("server started on port 80");
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+  console.log("HTTP Server running on port 80");
+});
+
+httpsServer.listen(443, () => {
+  console.log("HTTPS Server running on port 443");
 });

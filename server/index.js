630a442cd2fce0930c86a7c3107d8c83f439e508
atidele <alejandro.tidele@mercadolibre.com>
2020-06-21 14:36:34 -0300

    Palta Tech site has changed to a parallax site.

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express(); // create express app
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
+app.use(express.static(__dirname, { dotfiles: "allow" }));
 
 // start express server on port 80
 app.listen(80, () => {

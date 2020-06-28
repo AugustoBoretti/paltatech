6c26efb1abb354878615e85811c74d5761630beb
atidele <alejandro.tidele@mercadolibre.com>
2020-06-28 15:21:45 -0300

    Starting Our Works

--- src/pages/IndexSection/OurWorks.jsx
+++ src/pages/IndexSection/OurWorks.jsx
@@ -1,79 +1,45 @@
 import React from "react";
-import { Container } from "reactstrap";
-import IsometricGrid, { Cell } from "react-isometric-grid";
-import dynamics from "dynamics.js";
+import { Col, Container, Row } from "reactstrap";
+import Zoom from "react-reveal/Zoom";
 
 const OurWorks = ({ setRef }) => {
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  const images = [
+    "https://i.picsum.photos/id/696/500/1000.jpg?hmac=Y0G4wIBSwSkMEC1P-_KfSRjFPKsSY4CempEP5Y71_jQ",
+    "https://i.picsum.photos/id/831/500/1000.jpg?hmac=ATbgJ78fErDWHBK8YpXTEQn0baD8hBflnhEOGypQt5s",
+    "https://i.picsum.photos/id/524/500/1000.jpg?hmac=0yPSsAbk7plHp_K0g3sjgBC7Nyry4k6dOuck88c-U0M",
+    "https://i.picsum.photos/id/829/500/1000.jpg?hmac=ZgPKvvHodSmVDEYviIFEAm1zqPl3NDbICVJYXwOkj6w",
+    "https://i.picsum.photos/id/796/500/1000.jpg?hmac=ejcFiRfeUGuvyC-wSxLWhh9_6ZQlig5qn6AM_YChxwk",
+    "https://picsum.photos/500/1000",
+  ];
 
   return (
-    <div
-      className="section features-7"
-      style={{ minHeight: "100vh", marginTop: "125px" }}
-      ref={setRef}
-    >
+    <div className="section section-basic isometric-section" ref={setRef}>
       <Container>
-        <IsometricGrid
-          shadow
-          transform="rotateX(45deg) rotateZ(45deg)"
-          stackItemsAnimation={{
-            properties: function (pos) {
-              return {
-                translateZ: (pos + 1) * 30,
-                rotateZ: getRandomInt(-40, -50),
-              };
-            },
-            options: function (pos, itemstotal) {
-              return {
-                type: dynamics.easeIn,
-                duration: 700,
-                points: [
-                  { x: 0, y: 0, cp: [{ x: 0.2, y: 1 }] },
-                  { x: 1, y: 1, cp: [{ x: 0.3, y: 1 }] },
-                  { x: 3, y: 1, cp: [{ x: 0.9, y: 2 }] },
-                ],
-                delay: (itemstotal - pos - 1) * 40,
-              };
-            },
-          }}
-          style={{ height: "800px", width: "900px" }}
-        >
-          <Cell
-            layers={[
-              "https://demos.creative-tim.com/paper-kit-pro-react/static/media/pricing-card.105f38e3.JPG",
-              "#15ce89",
-              "#15ce89",
-              "#15ce89",
-            ]}
-          />
-          <Cell
-            className="hidden"
-            layers={[
-              "https://demos.creative-tim.com/paper-kit-pro-react/static/media/twitter-card.946883a5.JPG",
-              "#15ce89",
-              "#15ce89",
-              "#15ce89",
-            ]}
-          />
-          <Cell
-            layers={[
-              "https://demos.creative-tim.com/paper-kit-pro-react/static/media/team-card.35357561.JPG",
-              "#15ce89",
-              "#15ce89",
-              "#15ce89",
-            ]}
-          />
-          <Cell
-            layers={[
-              "https://demos.creative-tim.com/paper-kit-pro-react/static/media/blog-card.58808458.JPG",
-              "#15ce89",
-              "#15ce89",
-              "#15ce89",
-            ]}
-          />
-        </IsometricGrid>
+        <Row>
+          <Col className="ml-auto mr-auto" md="12">
+            <h1 className="title">
+              <Zoom left cascade>
+                Our
+              </Zoom>
+              <strong className="text-info">
+                <Zoom right cascade>
+                  Works
+                </Zoom>
+              </strong>
+            </h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="12">
+            {images.map((image, k) => (
+              <div key={k} className={`isometric-${k + 1}`}>
+                {new Array(4).fill(null).map((index, key) => (
+                  <img key={key} src={image} />
+                ))}
+              </div>
+            ))}
+          </Col>
+        </Row>
       </Container>
     </div>
   );

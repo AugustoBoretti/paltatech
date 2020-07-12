17f94109d8cc8c5ae1dbba2d3ce01f9b09ad51f1
atidele <alejandro.tidele@mercadolibre.com>
2020-07-12 11:07:13 -0300

    New michroma press

---
+++ server/config/Michroma/press.js
@@ -0,0 +1,422 @@
+const startups = {
+  MICHROMA: {
+    name: "MICHROMA",
+    url: "https://michroma.co/",
+  },
+  FEEDVAX: {
+    name: "FEEDVAX",
+    url: "https://www.feedvax.com/",
+  },
+  MICROGENESIS: {
+    name: "MICROGÉNESIS",
+    url: null,
+  },
+  TOMORROW_FOODS: {
+    name: "TOMORROW FOODS",
+    url: "https://tomorrowfoods.com.ar/",
+  },
+  GRIDX: {
+    name: "GRIDX",
+    url: "https://gridexponential.com/",
+  },
+  STAMM: {
+    name: "STÄMM",
+    url: "http://www.stamm.bio/",
+  },
+  BEEFLOW: {
+    name: "BEEFLOW",
+    url: "https://www.beeflow.com/",
+  },
+  BITGENIA: {
+    name: "BITGENIA",
+    url: "https://www.bitgenia.com/",
+  },
+  ALYTIX: {
+    name: "ALYTIX",
+    url: "http://alytixbiotech.com/",
+  },
+  APLIFE: {
+    name: "APLIFE",
+    url: "https://www.aplifebiotech.com/",
+  },
+  ZEV_BIOTECH: {
+    name: "ZEV BIOTECH",
+    url: "http://zevbiotech.com/",
+  },
+  CASPR: {
+    name: "CASPR",
+    url: "https://caspr.bio/",
+  },
+  ALGAEBIO: {
+    name: "ALGAEBIO+",
+    url: "http://algaebiomas.com/",
+  },
+  CELL_FARM: {
+    name: "CELL FARM",
+    url: "https://cellfarmfoodtech.com/",
+  },
+  MZP: {
+    name: "MZP",
+    url: "http://www.mzptec.com/",
+  },
+  FECUNDIS: {
+    name: "FECUNDIS",
+    url: "https://www.terrafecundis.com/",
+  },
+  SYOCIN: {
+    name: "SYOCIN",
+    url: "http://syocin.com/",
+  },
+  HIAMET: {
+    name: "HIAMET",
+    url: "https://www.hiamet.com/",
+  },
+  EINSTED: {
+    name: "EINSTED",
+    url: null,
+  },
+  CRYOSMETICS: {
+    name: "CRYOSMETICS",
+    url: "https://www.cryosmetics.com/",
+  },
+  ENTERIA: {
+    name: "ENTERIA",
+    url: null,
+  },
+};
+
+const press = [
+  {
+    id: 1,
+    people: ["Ricardo Cassini", "Mauricio Braia"],
+    startups: [startups.MICHROMA],
+    media: "Bioemprendiendo",
+    author: "Hector Garza Avelar",
+    name:
+      "Emprende para innovar, emprende con ganas con Ricky Cassini y Mauricio Braia de Michroma",
+    date: new Date("06/07/2020"),
+    link:
+      "https://anchor.fm/bioemprendiendo/episodes/025---Emprende-para-innovar--emprende-con-ganas-con-Ricky-Cassini-y-Mauricio-Braia-de-Michroma-eel192/a-a2ah0pq",
+    lang: "spanish",
+  },
+  {
+    id: 2,
+    people: ["Ricardo Cassini", "Mauricio Braia"],
+    startups: [startups.MICHROMA],
+    media: "WOW Innovación - Telefé",
+    author: "Antonela Losada",
+    name: "Michroma | Wow Innovación - Micro 102",
+    date: new Date("05/14/2020"),
+    link: "https://www.youtube.com/watch?v=dZRQ4CNvgPw",
+    isVideo: true,
+    lang: "english",
+  },
+  {
+    id: 3,
+    people: ["Ricardo Cassini", "Mauricio Braia"],
+    startups: [startups.MICHROMA],
+    media: "Salmon Expert",
+    author: "Daniella Balin",
+    name:
+      "Desarrollan colorante natural que podrá ser incluido en dietas de salmónidos",
+    date: new Date("05/05/2020"),
+    link:
+      "https://www.salmonexpert.cl/article/desarrollan-colorante-natural-en-base-a-hongos-para-ser-incluido-en-dietas-de-salmnidos/",
+    lang: "spanish",
+  },
+  {
+    id: 4,
+    people: ["Ricardo Cassini"],
+    startups: [startups.MICHROMA],
+    media: "Radio Andina",
+    author: "Autor desconocido",
+    name: "Todo por la Tarde - Ricky Cassini - Michroma",
+    date: new Date("04/16/2020"),
+    link:
+      "https://soundcloud.com/mediosandinosmza/todo-por-la-tarde-ricky-cassini-michroma-16-04-20",
+    lang: "spanish",
+  },
+  {
+    id: 5,
+    people: ["Ricardo Cassini"],
+    startups: [startups.MICHROMA],
+    media: "UC Cuyo",
+    author: "Leandro Frías",
+    name:
+      "Webinar: Impulsan el emprendedurismo y la investigación en la UCCuyo con un encuentro virtual",
+    date: new Date("03/31/2020"),
+    link:
+      "http://www.uccuyosj.edu.ar/index.php/la-universidad/institucional/noticias/1305-webinar-impulsan-el-emprendedurismo-y-la-investigacion-en-la-uccuyo-con-un-encuentro-virtual",
+    lang: "spanish",
+  },
+  {
+    id: 6,
+    people: ["Luis Barletta", "Ricardo Cassini", "Gabriela Gutiérrez"],
+    startups: [startups.FEEDVAX, startups.MICHROMA, startups.MICROGENESIS],
+    media: "La Nación",
+    author: "Jesús Allende",
+    name:
+      "Del laboratorio a la empresa: los científicos se ponen el traje de emprendedores",
+    date: new Date("02/22/2020"),
+    link:
+      "https://www.lanacion.com.ar/economia/del-laboratorio-a-la-empresa-los-cientificos-se-ponen-el-traje-de-emprendedores-nid2335674",
+    lang: "spanish",
+  },
+  {
+    id: 7,
+    people: ["Gonzalo Segovia", "Ricardo Cassini"],
+    startups: [startups.MICHROMA, startups.TOMORROW_FOODS],
+    media: "Clarín",
+    author: "Hernán Murúa",
+    name:
+      "5 startups que 'levantaron' US$ 2 millones antes de facturar un solo centavo",
+    date: new Date("02/20/2020"),
+    link:
+      "https://www.clarin.com/economia/5-startups-levantaron-us-2-millones-facturar-solo-centavo_0_EsWhJ4or.html",
+    lang: "spanish",
+  },
+  {
+    id: 8,
+    people: ["Ricardo Cassini"],
+    startups: [startups.MICHROMA],
+    media: "Medium",
+    author: "Po Bronson",
+    name: "Michroma: Reinventing Food Colors",
+    date: new Date("02/11/2020"),
+    link:
+      "https://medium.com/indiebio-sf/michroma-reinventing-food-colors-3cc5ee99f92f",
+    lang: "english",
+  },
+  {
+    id: 9,
+    people: ["Ricardo Cassini"],
+    startups: [startups.MICHROMA],
+    media: "Food Navigator",
+    author: "Niamh Michail",
+    name:
+      "Argentinian startup develops 'novel compounds' food colors from fungi",
+    date: new Date("02/06/2020"),
+    link:
+      "https://www.foodnavigator-latam.com/Article/2020/02/06/Novel-and-natural-food-coloring-developed-from-fungi-by-Silicon-Valley-start-up",
+    lang: "english",
+  },
+  {
+    id: 10,
+    people: null,
+    startups: [startups.MICHROMA],
+    media: "Synbiobeta",
+    author: "Desiree Ho",
+    name: "IndieBio’s Class Nine: Archetypes of Engineering Biology",
+    date: new Date("02/06/2020"),
+    link:
+      "https://synbiobeta.com/indiebios-class-nine-archetypes-of-engineering-biology/",
+    lang: "english",
+  },
+  {
+    id: 11,
+    people: ["Agustín Belloso", "Ricardo Cassini", "Matías Peire"],
+    startups: [startups.MICHROMA, startups.TOMORROW_FOODS, startups.GRIDX],
+    media: "Revista Pymes",
+    author: "Hernán Murúa",
+    name: "Startups de las que se va a hablar en el 2020",
+    date: new Date("02/01/2020"),
+    link: "https://drive.google.com/open?id=1kG3zGM9alWWLIuqx0GxkRCPdP1xB9qwC",
+    lang: "spanish",
+  },
+  {
+    id: 12,
+    people: ["Ricardo Cassini"],
+    startups: [startups.MICHROMA],
+    media: "The Spoon",
+    author: "Catherine Lamb",
+    name:
+      "Dye Another Day: Michroma Makes Sustainable Food Coloring through Fungi Fermentation",
+    date: new Date("01/23/2020"),
+    link:
+      "https://thespoon.tech/dye-another-day-michroma-makes-sustainable-food-coloring-through-funghi-fermentation/",
+    lang: "english",
+  },
+  {
+    id: 13,
+    people: null,
+    startups: [
+      startups.GRIDX,
+      startups.STAMM,
+      startups.BEEFLOW,
+      startups.BITGENIA,
+      startups.ALYTIX,
+      startups.APLIFE,
+      startups.ZEV_BIOTECH,
+      startups.CASPR,
+      startups.ALGAEBIO,
+      startups.FEEDVAX,
+      startups.CELL_FARM,
+      startups.MZP,
+      startups.MICHROMA,
+      startups.TOMORROW_FOODS,
+    ],
+    media: "Contxto",
+    author: "Mariana López",
+    name: "14 biotechs rewriting Argentina’s startup DNA",
+    date: new Date("01/15/2020"),
+    link:
+      "https://www.contxto.com/en/argentina/14-biotechs-rewriting-argentinas-startup-dna/",
+    lang: "english",
+  },
+  {
+    id: 14,
+    people: null,
+    startups: [
+      startups.GRIDX,
+      startups.STAMM,
+      startups.BEEFLOW,
+      startups.BITGENIA,
+      startups.ALYTIX,
+      startups.APLIFE,
+      startups.ZEV_BIOTECH,
+      startups.CASPR,
+      startups.ALGAEBIO,
+      startups.FEEDVAX,
+      startups.CELL_FARM,
+      startups.MZP,
+      startups.MICHROMA,
+      startups.TOMORROW_FOODS,
+    ],
+    media: "Contxto",
+    author: "Mariana López",
+    name: "14 biotechs rewriting Argentina’s startup DNA",
+    date: new Date("01/15/2020"),
+    link:
+      "https://www.contxto.com/en/argentina/14-biotechs-rewriting-argentinas-startup-dna/",
+    lang: "english",
+  },
+  {
+    id: 15,
+    people: null,
+    startups: [
+      startups.GRIDX,
+      startups.STAMM,
+      startups.BEEFLOW,
+      startups.BITGENIA,
+      startups.ALYTIX,
+      startups.APLIFE,
+      startups.ZEV_BIOTECH,
+      startups.CASPR,
+      startups.ALGAEBIO,
+      startups.FEEDVAX,
+      startups.CELL_FARM,
+      startups.MZP,
+      startups.MICHROMA,
+      startups.TOMORROW_FOODS,
+    ],
+    media: "Contxto",
+    author: "Mariana López",
+    name: "14 biotechs reescribiendo el ADN de startups argentinas",
+    date: new Date("01/15/2020"),
+    link:
+      "https://www.contxto.com/es/startups-es/14-biotechs-reescribiendo-adn-startups-argentino/",
+    lang: "spanish",
+  },
+  {
+    id: 16,
+    people: ["Matías Peire"],
+    startups: [
+      startups.GRIDX,
+      startups.MICHROMA,
+      startups.FECUNDIS,
+      startups.SYOCIN,
+    ],
+    media: "BCR News",
+    author: "Autor desconocido",
+    name:
+      "Llega un nuevo Workshop para emprendedores a la Bolsa de Comercio de Rosario",
+    date: new Date("10/28/2019"),
+    link:
+      "https://bcrnews.com.ar/innovacion/llega-un-nuevo-workshop-para-emprendedores-a-la-bolsa-de-comercio-de-rosario/",
+    lang: "spanish",
+  },
+  {
+    id: 17,
+    people: ["Arvind Gupta"],
+    startups: [
+      startups.GRIDX,
+      startups.STAMM,
+      startups.BEEFLOW,
+      startups.CASPR,
+      startups.MICHROMA,
+    ],
+    media: "Apertura Negocios",
+    author: "Ricardo Quesada",
+    name:
+      "El mayor fondo de biotecnología del mundo, a la caza de unicornios argentinos",
+    date: new Date("10/07/2019"),
+    link:
+      "https://www.cronista.com/apertura-negocio/emprendedores/El-mayor-fondo-de-biotecnologia-del-mundo-a-la-caza-de-unicornios-argentinos-20191007-0007.html",
+    lang: "spanish",
+  },
+  {
+    id: 18,
+    people: ["Matías Peire"],
+    startups: [
+      startups.GRIDX,
+      startups.STAMM,
+      startups.BEEFLOW,
+      startups.BITGENIA,
+      startups.ALYTIX,
+      startups.ZEV_BIOTECH,
+      startups.CASPR,
+      startups.ALGAEBIO,
+      startups.FEEDVAX,
+      startups.CELL_FARM,
+      startups.MZP,
+      startups.MICHROMA,
+      startups.TOMORROW_FOODS,
+      startups.HIAMET,
+    ],
+    media: "Agrofy News",
+    author: "Nicolás Degano",
+    name:
+      "Biotecnología: proyectos argentinos que pueden cambiar la forma de producir",
+    date: new Date("10/01/2019"),
+    link:
+      "https://news.agrofy.com.ar/noticia/183317/biotecnologia-proyectos-argentinos-que-pueden-cambiar-forma-producir",
+    lang: "spanish",
+  },
+  {
+    id: 19,
+    people: null,
+    startups: [
+      startups.GRIDX,
+      startups.MICHROMA,
+      startups.TOMORROW_FOODS,
+      startups.SYOCIN,
+      startups.EINSTED,
+      startups.MICROGENESIS,
+      startups.CRYOSMETICS,
+      startups.FECUNDIS,
+      startups.ENTERIA,
+      startups.HIAMET,
+    ],
+    media: "Argentina.gob.ar",
+    author: "Autor desconocido",
+    name: "Proyectos de base biotecnológica de impacto global en el planetario",
+    date: new Date("06/14/2019"),
+    link: "https://www.argentina.gob.ar/ciencia/reuniones/planetario",
+    lang: "spanish",
+  },
+  {
+    id: 20,
+    people: null,
+    startups: [startups.MICHROMA],
+    media: "Argentina.gob.ar",
+    author: "Autor desconocido",
+    name:
+      "Quince nuevos ganadores del Concurso Nacional PROESUS para emprendimientos sustentables",
+    date: new Date("06/14/2019"),
+    link:
+      "https://www.argentina.gob.ar/noticias/quince-nuevos-ganadores-del-concurso-nacional-proesus-para-emprendimientos-sustentables",
+    lang: "spanish",
+  },
+];
+
+module.exports = press;

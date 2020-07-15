b44264db0927cf10a7b39192200332836c6bcff3
atidele <alejandro.tidele@mercadolibre.com>
2020-07-15 17:23:46 -0300

    Https fixed 😅

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const https = require("https");
 const express = require("express");
 const app = express();
-//const credentials = require("./private/certification/siteCredentials");
+const credentials = require("./private/certification/siteCredentials");
 const emailRoutes = require("./src/routes/emailRoute");
 const michromaRoutes = require("./src/routes/michromaRoute");
 
@@ -30,12 +30,12 @@ app.use(express.static(__dirname, { dotfiles: "allow" }));
 
 // Starting both http & https servers
 const httpServer = http.createServer(app);
-//const httpsServer = https.createServer(credentials, app);
+const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80, () => {
   console.log("HTTP Server running on port 80 🚀");
 });
 
-/*httpsServer.listen(443, () => {
+httpsServer.listen(443, () => {
   console.log("HTTPS Server running on port 443 🚀");
-});*/
+});

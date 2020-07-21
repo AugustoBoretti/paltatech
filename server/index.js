543376b98c56ee6a8623fb76da845e9f9a4b932c
atidele <alejandro.tidele@mercadolibre.com>
2020-07-21 11:14:00 -0300

    Test michroma http.

--- server/index.js
+++ server/index.js
@@ -3,16 +3,36 @@ const http = require("http");
 const https = require("https");
 const express = require("express");
 const app = express();
-const michromaApp = express();
 const credentials = require("./private/certification/siteCredentials");
 const emailRoutes = require("./src/routes/emailRoute");
-const michromaRoutes = require("./src/routes/michromaRoute");
 
 // add middlewares
 
 app.use("/static", express.static(path.join(__dirname, "..", "build/static")));
+app.use(
+  "/michroma/static",
+  express.static(path.join(__dirname, "pages/michroma/static"))
+);
+app.use(
+  "/michroma/press/static",
+  express.static(path.join(__dirname, "pages/michroma/press/static"))
+);
 app.use("/email", emailRoutes);
 app.get("*", function (req, res) {
+  switch (req.headers.host) {
+    case "michroma.co": {
+      if (req.url === "/press") {
+        return res.sendFile("index.html", {
+          root: path.join(__dirname, "pages/michroma/press"),
+        });
+      }
+
+      return res.sendFile("index.html", {
+        root: path.join(__dirname, "pages/michroma"),
+      });
+    }
+  }
+
   res.sendFile("index.html", {
     root: path.join(__dirname, "..", "build"),
   });
@@ -20,21 +40,9 @@ app.get("*", function (req, res) {
 app.use(express.static("public"));
 app.use(express.static(__dirname, { dotfiles: "allow" }));
 
-michromaApp.use(
-    "/michroma/static",
-    express.static(path.join(__dirname, "pages/michroma/static"))
-);
-michromaApp.use(
-    "/michroma/press/static",
-    express.static(path.join(__dirname, "pages/michroma/press/static"))
-);
-michromaApp.use("/", michromaRoutes);
-
 // Starting both http & https servers
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
-const michromaHttpServer = http.createServer(michromaApp);
-const michromaHttpsServer = https.createServer(michromaApp, app);
 
 httpServer.listen(80, () => {
   console.log("HTTP Server running on port 80 🚀");
@@ -42,12 +50,4 @@ httpServer.listen(80, () => {
 
 httpsServer.listen(443, () => {
   console.log("HTTPS Server running on port 443 🚀");
-});
-
-michromaHttpServer.listen(8081, () => {
-  console.log("HTTP Server running on port 80 🚀");
-});
-
-michromaHttpsServer.listen(8082, () => {
-  console.log("HTTP Server running on port 80 🚀");
 });
